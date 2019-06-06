7a0b1a65c1dfa916b574f0cc8552bdeb7a254b82
Ksenia <ksenia.krasilschikova@veeam.com>
2019-06-06 16:59:54 +0300

    fix bfs

--- breadthfirstsearch/breadthfirstsearch.go
+++ breadthfirstsearch/breadthfirstsearch.go
@@ -1,7 +1,7 @@
 package breadthfirstsearch
 
 /*
-BreadthFirstSearch finds the shortest path between 2 points on graph it it exists
+BreadthFirstSearch finds the shortest path between 2 points on graph if it exists
 */
 func BreadthFirstSearch(graph map[string][]string, start string, finish string) bool {
 	queue := graph[start]
